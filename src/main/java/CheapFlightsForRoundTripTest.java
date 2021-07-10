feaa910a3d2bb2e864862cb29eba8fc905173fb2
“masudulhas” <masudulhas@gmail.com>
2021-07-10 01:36:44 +0100

    correct allignment

---
+++ src/main/java/CheapFlightsForRoundTripTest.java
@@ -0,0 +1,5 @@
+public class CheapFlightsForRoundTripTest {
+    public static void main(String[] args) {
+        
+    }
+}
--- src/main/java/Selenium.java
+++ src/main/java/Selenium.java
@@ -6,30 +6,33 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.concurrent.TimeUnit;
 public class Selenium {
     public static void main(String[] args) {
+
         // Locators
         By loginLinkLocator = By.className("account-title");
         By cookiePopUpLocator =  By.className("css-47sehv");
-        //working here
-       By loginUserNameInputLocator = By.xpath("//input[contains(@placeholder, 'Your e-mail')]");
-       By loginPasswordInputLocator = By.xpath("//input[contains(@type, 'password')]");
-       By loginButtonLocator = By.xpath("//button[contains(@type, 'submit')]");
 
-        // creating current direction
+        //Working here
+        By loginUserNameInputLocator = By.xpath("//input[contains(@placeholder, 'Your e-mail')]");
+        By loginPasswordInputLocator = By.xpath("//input[contains(@type, 'password')]");
+        By loginButtonLocator = By.xpath("//button[contains(@type, 'submit')]");
+
+        //Creating current direction
         String currentDir = System.getProperty("user.dir");
         System.out.println("Current dir using System:" + currentDir);
         System.setProperty("webdriver.chrome.driver", currentDir + "\\src\\main\\resources\\driver\\chromedriver.exe");
 
-        // Notification disable
+        //Notification disable
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications");
 
-        // creating driver object
+        //Creating driver object
         ChromeDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize(); //Maximize window
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         //setting up url
         String baseUrl = "https://www.esky.co.uk";
+
         // launch chrome and execute the test steps
         driver.get(baseUrl);
         driver.findElement(cookiePopUpLocator).click();
@@ -39,27 +42,28 @@ public class Selenium {
         driver.findElement(loginPasswordInputLocator).sendKeys("62320702Asba..");
         driver.findElement(loginButtonLocator).click();
         driver.switchTo().defaultContent();
+
         // get the actual value of the title
         String actualTitle = driver.getTitle();
         System.out.println("actual title is: " + actualTitle);
         String expectedTitle = "eSky.co.uk - Flights, Airline Tickets, Flight Search, Deals";
 
 
-        /*
-         * compare the actual title of the page with the expected one and print
-         * the result as "Passed" or "Failed"
-         */
+        //Compare the actual title of the page with the expected one and print
+        //The result as "Passed" or "Failed"
         if (actualTitle.contentEquals(expectedTitle)) {
             System.out.println("Test Passed!");
         } else {
             System.out.println("Test Failed");
         }
+
         // sleeping the execution for 4000 milly sec
         try {
             Thread.sleep(4000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
         //close chrome
         //driver.close();
     }
